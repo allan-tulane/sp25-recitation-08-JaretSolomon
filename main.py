ecb7391eb444c01d33f3dbfd21a4ff7c400907f5
Jaret Solomon <jsolomon4@tulane.edu>
2025-05-07 19:44:21 -0500

    Finsihed

--- main.py
+++ main.py
@@ -2,30 +2,39 @@ from collections import deque
 from heapq import heappush, heappop 
 
 def shortest_shortest_path(graph, source):
-    """
-    Params: 
-      graph.....a graph represented as a dict where each key is a vertex
-                and the value is a set of (vertex, weight) tuples (as in the test case)
-      source....the source node
-      
-    Returns:
-      a dict where each key is a vertex and the value is a tuple of
-      (shortest path weight, shortest path number of edges). See test case for example.
-    """
-    ### TODO
-    pass
+  pq = [(0, 0, source)] 
+  visited = {}
+
+  while pq:
+      weight, edges, node = heappop(pq)
+      if node in visited:
+          previous_weight, previous_edges = visited[node]
+          if weight > previous_weight or (weight == previous_weight and edges >= previous_edges):
+              continue
+
+      visited[node] = (weight, edges)
+
+      for neighbor, w in graph.get(node, []):
+          new_weight = weight + w
+          n_edges = edges + 1
+          heappush(pq, (new_weight, n_edges, neighbor))
+
+  return visited
+    
     
 
-    
-    
+
 def bfs_path(graph, source):
-    """
-    Returns:
-      a dict where each key is a vertex and the value is the parent of 
-      that vertex in the shortest path tree.
-    """
-    ###TODO
-    pass
+  parents = {source: None}
+  queue = deque([source])
+  while queue:
+      current = queue.popleft()
+      for neighbors in graph.get(curr, []):
+          if neighbors not in parents:
+              parents[neighbors] = curr
+              queue.append(neighbors)
+
+  return parents
 
 def get_sample_graph():
      return {'s': {'a', 'b'},
@@ -38,11 +47,10 @@ def get_sample_graph():
 
     
 def get_path(parents, destination):
-    """
-    Returns:
-      The shortest path from the source node to this destination node 
-      (excluding the destination node itself). See test_get_path for an example.
-    """
-    ###TODO
-    pass
-
+  path = []
+  curr = destination
+  while current is not None:
+      path.append(curr)
+      current = parents.get(curr)
+  path.reverse()
+  return ''.join(path[:-1])
